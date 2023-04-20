18982a743a9fdd273a2a55893707738bdb9d9db0
Praveen Rao V P <praveenraovp01@gmail.com>
2023-04-20 14:50:42 +0530

    casting

---
+++ Day1/Casting.java
@@ -0,0 +1,9 @@
+public class Casting {
+    public static void main(String[] args) {
+        int intVariable = 9;
+		double doubleVariable = intVariable; 
+	
+		System.out.println(intVariable);      
+		System.out.println(doubleVariable); 
+    }
+}
