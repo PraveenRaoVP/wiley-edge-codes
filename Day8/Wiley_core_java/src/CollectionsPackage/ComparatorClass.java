0b47541192b21a1342f9425eb98a215690c32ef0
Praveen Rao V P <praveenraovp01@gmail.com>
2023-05-02 17:03:19 +0530

    comparator

---
+++ Day8/Wiley_core_java/src/CollectionsPackage/ComparatorClass.java
@@ -0,0 +1,28 @@
+package CollectionsPackage;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class MyClass implements Comparator<Integer>{
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+//		if(o1>o2) {
+//			return -1;
+//		} else if(o1<o2) {
+//			return 1;
+//		}
+		return o1.compareTo(o2);
+	}
+	
+}
+
+public class ComparatorClass {
+	public static void main(String[] args) {
+		TreeSet<Integer> ts = new TreeSet<>(new MyClass());
+		ts.add(4);
+		ts.add(1);
+		ts.add(2);
+		System.out.println(ts);
+	}
+}
