a49c9333ce02fc6029d56bb333cec2208af934d8
Praveen Rao V P <praveenraovp01@gmail.com>
2023-05-12 15:10:59 +0530

    binary tree views

--- Day11/Wiley_core_java/src/hashing/BinaryTreeBottomView.java
+++ Day11/Wiley_core_java/src/hashing/BinaryTreeBottomView.java
@@ -1,10 +1,6 @@
 package hashing;
 
 import java.util.*;
-//import java.util.*;
-
-
-
 
 public class BinaryTreeBottomView {
     public static void printBottomView(Node root) {
@@ -34,15 +30,6 @@ public class BinaryTreeBottomView {
     }
 
     public static void main(String[] args) {
-        /*
-                    1
-                   / \
-                  2   3
-                 / \   \
-                4   5   6
-                     \
-                      7
-        */
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
@@ -57,26 +44,3 @@ public class BinaryTreeBottomView {
 }
 
 
-
-
-/*
- * we define a TreeNodeTwo class representing a node in the binary tree. Each
- * TreeNodeTwo object has a value (val) and left and right child pointers.
- * 
- * The printBottomView method takes the root of the binary tree as input and
- * prints the bottom view of the tree. It uses a map (TreeMap) to store the last
- * encountered node at each horizontal distance (hd) from the root. It also uses
- * a queue to perform a level-order traversal of the tree.
- * 
- * During the traversal, for each node, we update the value in the map with the
- * current node's value at the corresponding horizontal distance. We then
- * enqueue the left and right child nodes (if they exist) along with their
- * updated horizontal distances.
- * 
- * Finally, we iterate over the map values and print them, which will give us
- * the bottom view of the binary tree.
- * 
- * In the main method, we create a binary tree with the values shown in the
- * comment above the tree. We then call the printBottomView method to print the
- * bottom view of the binary tree.
- */
