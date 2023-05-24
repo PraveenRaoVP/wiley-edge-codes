b75465dfc8ebea778c5a9764283fe6aa35a7ac4f
Praveen Rao V P <praveenraovp01@gmail.com>
2023-05-24 09:45:07 +0530

    procedures in jdbc

--- MySQLCodes/triggers.sql
+++ MySQLCodes/triggers.sql
@@ -7,4 +7,7 @@ name VARCHAR(45) NULL,
 salary INT NULL,
 age INT NULL,
 PRIMARY KEY (id));
-select * from employee;
+select * from candidate_skills;
+
+DELIMITER $$ CREATE PROCEDURE get_candidate_skills() BEGIN SELECT first_name, last_name, skills.skill_id as skill FROM candidates, candidate_skills as skills; END $$ DELIMITER;
+
